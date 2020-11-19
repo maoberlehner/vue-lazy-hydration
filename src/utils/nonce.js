61bbb20c26235dfe6db7bc7e30e3d542acc6375d
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-11-19 10:21:27 +0100

    Remove unnecessary code
    Manually Resolving the async factory component is not necessary.

--- src/utils/nonce.js
+++ src/utils/nonce.js
@@ -1,20 +1,9 @@
 const isServer = typeof window === `undefined`;
 
-function isAsyncComponentFactory(componentOrFactory) {
-  return typeof componentOrFactory === `function`;
-}
-
-function resolveComponent(componentOrFactory) {
-  if (isAsyncComponentFactory(componentOrFactory)) {
-    return componentOrFactory().then(componentModule => componentModule.default);
-  }
-  return componentOrFactory;
-}
-
 export function makeNonce({ component, hydrate, hydrationPromise }) {
   return () => new Promise((resolve) => {
     if (isServer) hydrate();
 
-    hydrationPromise.then(() => resolve(resolveComponent(component)));
+    hydrationPromise.then(() => resolve(component));
   });
 }
