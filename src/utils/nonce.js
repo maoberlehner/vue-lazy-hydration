f715bbe70e7e3d81c69d055807417fd43457e433
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-11-19 17:42:02 +0100

    Fix rendering async wrapped components
    Fixes #79

--- src/utils/nonce.js
+++ src/utils/nonce.js
@@ -1,7 +1,18 @@
 const isServer = typeof window === `undefined`;
 
+function isAsyncComponentFactory(componentOrFactory) {
+  return typeof componentOrFactory === `function`;
+}
+
+function resolveComponent(componentOrFactory) {
+  if (isAsyncComponentFactory(componentOrFactory)) {
+    return componentOrFactory().then(componentModule => componentModule.default);
+  }
+  return componentOrFactory;
+}
+
 export function makeNonce({ component, hydrationPromise }) {
   if (isServer) return component;
 
-  return () => hydrationPromise.then(() => component);
+  return () => hydrationPromise.then(() => resolveComponent(component));
 }
