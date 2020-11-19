667c3c41ad19b27ebc66087e96ed3d0146c38da0
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-11-19 10:54:47 +0100

    Remove redundant promise

--- src/utils/nonce.js
+++ src/utils/nonce.js
@@ -3,7 +3,5 @@ const isServer = typeof window === `undefined`;
 export function makeNonce({ component, hydrationPromise }) {
   if (isServer) return component;
 
-  return () => new Promise((resolve) => {
-    hydrationPromise.then(() => resolve(component));
-  });
+  return () => hydrationPromise.then(() => component);
 }
